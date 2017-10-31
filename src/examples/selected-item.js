096874257e1a9964b42163b1de52657d1f5862c6
afeiship <1290657123@qq.com>
2017-10-31 15:11:13 +0800

    issue: bugfix

--- src/examples/selected-item.js
+++ src/examples/selected-item.js
@@ -11,11 +11,15 @@ export default class extends React.PureComponent {
     this.setState({disabled: !this.state.disabled})
   }
 
+  _onClick = e =>{
+    console.log('click!!');
+  };
+
   render() {
     return (
       <div className="example-selected-item">
         <button onClick={this._click1.bind(this)}>Toggle</button>
-        <ReactSelectedItem nodeName='button' selected={false} disabled={this.state.disabled}> Toggle
+        <ReactSelectedItem nodeName='button' selected={false} disabled={this.state.disabled} onClick={this._onClick}> Toggle
           Result - {String(this.state.disabled)} </ReactSelectedItem>
       </div>
     );
